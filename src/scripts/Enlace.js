07f0632bc741bce08ac9cb790f504234d4cf0b0f
mau611 <mauricio611rp@gmail.com>
2023-11-05 11:36:26 -0400

    asdasd

--- src/scripts/Enlace.js
+++ src/scripts/Enlace.js
@@ -1,8 +1,8 @@
-export var enlace = "http://localhost:8000/api";
+//export var enlace = "http://localhost:8000/api";
 //export var enlace = "https://api.shantispawellnesslife.com/api"
 //export var enlace = "https://cbapi.shantispawellnesslife.com/api"
-//export var enlace = "https://api.stilettofisio.com/api"
-export var enlace2 = "http://localhost:8000";
-//export var enlace = "https://api.shantispawellnesslife.com"
-//export var enlace = "https://cbapi.shantispawellnesslife.com"
-//export var enlace = "https://api.stilettofisio.com"
+export var enlace = "https://api.stilettofisio.com/api";
+//export var enlace2 = "http://localhost:8000";
+//export var enlace2 = "https://api.shantispawellnesslife.com"
+//export var enlace2 = "https://cbapi.shantispawellnesslife.com"
+export var enlace2 = "https://api.stilettofisio.com";
